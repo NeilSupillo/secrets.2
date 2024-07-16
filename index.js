05dd80c5f81f841fd8af5758eccc8a809d650634
Neil Supillo <neilsupillo@gmail.com>
2024-07-16 14:46:35 +0800

    indx

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"));
-app.set("views", __dirname + "/views");
+app.use(express.static("public"));
+app.set("views", "views");
 app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
