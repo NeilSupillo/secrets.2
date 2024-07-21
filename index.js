ce116b2e3039005e70d7bc5f755e673b3d564a5a
Neil Supillo <neilsupillo@gmail.com>
2024-07-21 14:05:04 +0800

    f

--- index.js
+++ index.js
@@ -53,17 +53,17 @@ app.use(passport.session());
 
 const { Pool } = pg;
 
-// const db = new Pool({
-//   connectionString: process.env.POSTGRES_URL,
-// });
-
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+const db = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
+
+// const db = new pg.Client({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
 db.connect();
 
 app.get("/", async (req, res) => {
